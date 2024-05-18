0987069cecdcca05d11f91b1785d77210099c97a
Ngoc Quy Le <nqle@mail.de>
2024-05-18 20:09:23 +0200

    manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/freshtrack/"
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "FreshTrack",
+        short_name: "FreshTrack",
+        description: "A simple app to track when perishables go bad",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "icon-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+        start_url: "/freshtrack/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#111111",
+      },
+    }),
+  ],
+  base: "/freshtrack/",
+});
