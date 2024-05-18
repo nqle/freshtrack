7bfb04a806f22b81575bca0f08f21744c666a66e
Ngoc Quy Le <nqle@mail.de>
2024-05-18 03:15:35 +0200

    listgroup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 // import Message from "./components/Message";
+import { useEffect } from "react";
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  const listGroupItems = ["banana", "apple", "spaghetti", "pho"];
+  const listGroupHeading = "Food";
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+    useEffect(() => {
+      alert(item);
+    });
+  };
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup
+        items={listGroupItems}
+        heading={listGroupHeading}
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,13 @@
-import { TouchList } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-  const items = ["banana", "apple", "spaghetti", "pho"];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedItemIdx, setSelectedItemIdx] = useState(-1);
 
   if (items.length === 0) {
     return (
@@ -17,11 +23,22 @@ function ListGroup() {
 
   return (
     <>
-      <h1>Items</h1>
+      <h1>{heading}</h1>
       {getMessage()}
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.map((item, idx) => (
+          <li
+            className={
+              selectedItemIdx === idx
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={"list-item-" + idx}
+            onClick={() => {
+              setSelectedItemIdx(idx);
+              onSelectItem(item);
+            }}
+          >
             {item}
           </li>
         ))}
