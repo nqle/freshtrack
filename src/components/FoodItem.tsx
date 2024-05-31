6573d0cf0c70a0155352a3df76454d1aca9fbd2a
Ngoc Quy Le <nqle@mail.de>
2024-06-01 01:39:28 +0200

    buttonize food icon wip

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -9,16 +9,22 @@ function FoodItem({ iconSrc, title, date }: Props) {
     <li className="list-group-item">
       <div className="row align-items-center">
         <div className="col-2">
-          {iconSrc && (
-            <img src={iconSrc} alt="Icon" className="img-fluid img-thumbnail" />
-          )}
-          {!iconSrc && (
-            <img
-              src="generic-fooditem-1024x1024.png"
-              alt="Icon"
-              className="img-fluid img-thumbnail"
-            />
-          )}
+          <button className="btn btn-default">
+            {iconSrc && (
+              <img
+                src={iconSrc}
+                alt="Icon"
+                className="img-fluid img-thumbnail"
+              />
+            )}
+            {!iconSrc && (
+              <img
+                src="generic-fooditem-1024x1024.png"
+                alt="Icon"
+                className="img-fluid img-thumbnail"
+              />
+            )}
+          </button>
         </div>
         <div className="col">{title}</div>
         <div className="col">{date?.toLocaleDateString()}</div>
